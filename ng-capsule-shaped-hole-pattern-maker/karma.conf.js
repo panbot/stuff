2646abe17eaea295fb53aca82679c9b69c327ce6
Botao Pan <botao_pan@gtfund.com>
2022-09-07 16:52:10 +0800

    rename

--- ng-capsule-shaped-hole-pattern-maker/karma.conf.js
+++ ng-capsule-shaped-hole-pattern-maker/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hemes-capsule-hole-pattern-maker'),
+      dir: require('path').join(__dirname, './coverage/ng-capsule-shaped-hole-pattern-maker'),
       subdir: '.',
       reporters: [
         { type: 'html' },
